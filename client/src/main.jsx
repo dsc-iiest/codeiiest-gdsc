99ab4e942fac461fd281bd9f03a3452c4f08dba2
Abhijit <artistrup07@gmail.com>
2024-03-19 22:55:36 +0530

    [BUILD] - created components: namecard(s), leaderboard

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import Leaderboard from './components/leaderboard/leaderboard.jsx'
+import Namecard from './components/namecard/namecard.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
