c88c93f6131c66adeebb7a7f84f8ed6ad89d382e
Siddharth Sen <sensiddharth093@gmail.com>
2024-06-03 13:38:15 +0530

    [UPDATE]: Modified the Codercard.css file by adding positioning the profile image and opacity to the background

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import About from "./screens/AboutUs/about";
-import {NameCard} from "./components/namecard/namecard"
+import { NameCard } from "./components/namecard/namecard"
+import CP_Leaderboard2 from "./screens/CP_Leaderboard/CP_Leaderboard2";
+import LeaderboardPage from './screens/CP_Leaderboard/LeaderboardPage';
 
 const AllRoutes = () => {
 	const location = useLocation();
@@ -17,6 +19,18 @@ const AllRoutes = () => {
 				exact
 				path="/"
 				element={<NameCard />}
+			>
+			</Route>
+			<Route
+				exact
+				path='/cp_leaderboard'
+				element={<CP_Leaderboard2></CP_Leaderboard2>}
+			>
+			</Route>
+			<Route
+				exact
+				path='cp_leaderboard/leaderboard'
+				element={<LeaderboardPage></LeaderboardPage>}
 			>
 			</Route>
 			<Route
