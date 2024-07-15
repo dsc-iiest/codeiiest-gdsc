4492538f1b115f1f3d798a6c3e16037d4d393b46
Rishab Dugar <www.anitadugar9163@gmail.com>
2024-07-15 14:38:33 +0530

    [BUILD]: added page background component

---
+++ client/src/components/PageBackground/PageBackground.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const useBackground = (page) => {
+	switch page{
+		case 'development': {
+			
+		}
+	}
+};
+
+const PageBackground = ({ page }) => {
+	
+	return (
+		<div className="bg">
+			<img src="/assets/bg/home_bg.png" alt="" />
+		</div>
+	);
+}
+ 
+export default PageBackground;
