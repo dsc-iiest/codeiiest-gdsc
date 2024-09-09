5f2ddb79bf9f822abc81042dd373244cf1192df4
“AdityaRaj150” <adityaraj10544@gmail.com>
2024-09-09 22:52:21 +0530

    [fix]: fixed ChapterContent.css a/c to figma design

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./screens/Home/Home";
+import About from "./screens/AboutUs/about"
+import Chapters from "./screens/Chapters/Chapters";
+import Chapterpage from "./screens/Chapterpage/Chapterpage";
+
+import CP_Leaderboard2 from "./screens/CP_Leaderboard/CP_Leaderboard2";
 // import Chapterpage from "./screens/Chapterpage/Chapterpage";
 // import CP_Leaderboard2 from "./screens/CP_Leaderboard/CP_Leaderboard2";
 // import Chapters from "./screens/Chapters/Chapters";
@@ -20,11 +25,11 @@ const AllRoutes = () => {
             <Route exact path="/" element={<Home></Home>}></Route>
             <Route exact path="/cp_leaderboard_page" element={<ComingSoon />}></Route>
             <Route exact path="/chapters">
-                {/* <Route path=":id" element={<Chapterpage></Chapterpage>}></Route> */}
-                <Route path="" element={<ComingSoon />}></Route>
+                <Route path=":id" element={<Chapterpage></Chapterpage>}></Route>
+                <Route path="" element={<Chapters/>}></Route>
             </Route>
-            <Route exact path="/about" element={<ComingSoon />}></Route>
-            <Route exact path="/leaderboard" element={<ComingSoon />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/leaderboard" element={<CP_Leaderboard2/>}></Route>
         </Routes>
     );
 };
