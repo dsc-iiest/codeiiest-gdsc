2df2cc63d9ab97c4b55c69f843d7b263d0bdcd0b
Siddharth Sen <sensiddharth093@gmail.com>
2024-11-05 00:30:17 +0530

    [UPDATE]: Added the data of chapters.json into the about.jsx file

--- client/src/screens/AboutUs/about.jsx
+++ client/src/screens/AboutUs/about.jsx
@@ -1,108 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import "./about.css";
 import NamecardV from "../../components/namecard/NameCardV";
 import Carousel from "../../components/Carousel/Carousel";
 import PageHeading from "../../components/PageHeading/PageHeading";
-// import { Box } from "@mui/material";
+import chaptersData from '../../assets/data/chapters.json';
 
-const members = [
-	<NamecardV
-		name={"Nafis Adnan Mondal"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafis Adnan"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafis Adnan Mondal 2000"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafi"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafis Mondal 6"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafis Adnan Mondal 7"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafis Adnan Mondal 8"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafis Adnan Mondal 9"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-	<NamecardV
-		name={"Nafis Adnan Mondal 10"}
-		codeiiest={"Development Lead"}
-		gdsc={"Development Lead"}
-		profilepic={"profilepic"}
-		descrip={
-			"Building the future, one component at a time"
-		}
-	/>,
-];
+const members = chaptersData.chapters[0].chapterContent.coreMembers.map((member) => (
+  <NamecardV
+    key={member.name} 
+    name={member.name}
+    codeiiest={member.role_in_codeiiest}
+    gdsc={member.role_in_gdg}
+    profilepic={member.image}
+    descrip={member.tagline}
+  />
+));
 
 const About = () => {
-	return (
-		<div className="about-wrapper page">
-			<div className="bg">
-				<img src="/assets/bg/home_bg.png" alt="" />
-			</div>
-			<PageHeading text="THE TEAM" />
-			{/* <div className="outer-content"> */}
-			{/* <div className="carousel-wrapper"> */}
-				<Carousel items={members} />
-			{/* </div> */}
-			{/* </div> */}
-		</div>
-	);
+  return (
+    <div className="about-wrapper page">
+      <div className="bg">
+        <img src="/assets/bg/home_bg.png" alt="" />
+      </div>
+      <PageHeading text="THE TEAM" />
+      <Carousel items={members} />
+    </div>
+  );
 };
 
 export default About;
