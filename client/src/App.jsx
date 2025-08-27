3457c6c71306bc1da828a466764e22a34084a94d
Soham Haldar <115897153+Mahos-H@users.noreply.github.com>
2025-08-27 23:33:46 +0530

    Updated
    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,8 @@ import { Analytics } from "@vercel/analytics/react";
 
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const App = () => {
